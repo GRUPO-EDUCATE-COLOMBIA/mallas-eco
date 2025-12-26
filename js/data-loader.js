4d0d800c686a5ad16ce34201167af55d5472117a
Grupoeducate <pedagogia@educateparaelsaber.edu.co>
2025-12-26 10:34:37 -0500

    Update data-loader.js

--- js/data-loader.js
+++ js/data-loader.js
@@ -1,18 +1,28 @@
 // js/data-loader.js
 
+// Estructura global de datos
 window.MallasData = {
-  matematicas_5: null
+  matematicas: {}
 };
 
-(function cargarMatematicas5() {
-  fetch('data/matematicas_5.json')
-    .then(r => r.json())
+// Carga una malla de Matemáticas por grado (1 a 5, por ahora)
+function cargarMallaMatematicas(grado) {
+  return fetch(`data/matematicas_${grado}.json`)
+    .then(r => {
+      if (!r.ok) throw new Error(`HTTP ${r.status}`);
+      return r.json();
+    })
     .then(json => {
-      window.MallasData.matematicas_5 = json;
-      console.log('Malla Matemáticas 5° cargada');
+      window.MallasData.matematicas[grado] = json;
+      console.log(`Malla Matemáticas ${grado}° cargada (tipo: ${json.tipo_malla}, períodos: ${json.numero_periodos})`);
     })
     .catch(err => {
-      console.error('Error cargando matematicas_5.json', err);
-      alert('No se pudo cargar la malla de Matemáticas 5°');
+      console.error(`Error cargando matematicas_${grado}.json`, err);
     });
-})();
+}
+
+// Cargar grados 1° a 5°
+Promise.all([1, 2, 3, 4, 5].map(g => cargarMallaMatematicas(g)))
+  .then(() => {
+    console.log('Mallas de Matemáticas 1°–5° cargadas');
+  });
